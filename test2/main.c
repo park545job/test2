66000ffa0464978e9cd445c6dbfe772b609e3a1b
park545job <job545job545@gmail.com>
2023-01-09 21:40:35 +0900

    Initial Commit

---
+++ test2/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  test2
+//
+//  Created by 박기남 on 2023/01/09.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
